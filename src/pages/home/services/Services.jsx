02897000f93b1ed24b3f25349ae52319d60d3bbd
jihad-hossain1 <jihadkhan934@gmail.com>
2023-05-23 11:13:24 +0600

    sort by decending & ascending

--- src/pages/home/services/Services.jsx
+++ src/pages/home/services/Services.jsx
@@ -3,15 +3,16 @@ import ServiceCard from "./ServiceCard";
 
 const Services = () => {
   const [services, setServices] = useState([]);
+  const [asc, setAsc] = useState(true);
 
   useEffect(() => {
-    fetch("http://localhost:5000/services")
+    fetch(`http://localhost:5000/services?sort=${asc ? "asc" : "desc"}`)
       .then((res) => res.json())
       .then((data) => {
         setServices(data);
         console.log(data);
       });
-  }, []);
+  }, [asc]);
   return (
     <div className="mt-8">
       <div className="text-center space-y-5">
@@ -22,17 +23,17 @@ const Services = () => {
           unde voluptatem necessitatibus <br /> deserunt voluptate quam aut
           numquam, iure ullam doloremque!
         </p>
+        <button onClick={() => setAsc(!asc)} className="btn btn-sm btn-primary">
+          {asc ? "Price High to Low" : "Price Low to High"}
+        </button>
       </div>
       <div>
-              <h4 className="text-xl">Total Services: {services.length}</h4>
-              <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
-                  {
-                      services.map(service => <ServiceCard
-                          key={service._id}
-                          service={service}
-                      ></ServiceCard>)
-                  }
-              </div>
+        <h4 className="text-xl">Total Services: {services.length}</h4>
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+          {services.map((service) => (
+            <ServiceCard key={service._id} service={service}></ServiceCard>
+          ))}
+        </div>
       </div>
     </div>
   );
